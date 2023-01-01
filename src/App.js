12368e9f738f1b9b7a8f05eba7153f327c52889c
oluwafisayomi <wrld26993@gmail.com>
2023-01-02 00:04:20 +0100

    [Feat] made the audio image a disc like cassette and it also spins

--- src/App.js
+++ src/App.js
@@ -20,29 +20,29 @@ function App() {
 
   const currentAudio = useRef()
 
-  const handleMusicProgressBar = (e)=>{
+  const handleMusicProgressBar = (e) => {
     setAudioProgress(e.target.value);
     currentAudio.current.currentTime = e.target.value * currentAudio.current.duration / 100;
   }
 
   //Change Avatar Class
-  let avatarClass = ['objectFitCover','objectFitContain','none']
+  let avatarClass = ['objectFitCover', 'objectFitContain', 'none']
   const [avatarClassIndex, setAvatarClassIndex] = useState(0)
-  const handleAvatar = ()=>{
+  const handleAvatar = () => {
     if (avatarClassIndex >= avatarClass.length - 1) {
       setAvatarClassIndex(0)
-    }else{
+    } else {
       setAvatarClassIndex(avatarClassIndex + 1)
     }
   }
 
 
   //Play Audio Function
-  const handleAudioPlay = ()=>{
+  const handleAudioPlay = () => {
     if (currentAudio.current.paused) {
       currentAudio.current.play();
       setIsAudioPlaying(true)
-    }else{
+    } else {
       currentAudio.current.pause();
       setIsAudioPlaying(false)
     }
@@ -93,31 +93,31 @@ function App() {
     }
   ]
 
-  const handleNextSong = ()=>{
+  const handleNextSong = () => {
     if (musicIndex >= musicAPI.length - 1) {
       let setNumber = 0;
       setMusicIndex(setNumber);
       updateCurrentMusicDetails(setNumber);
-    }else{
+    } else {
       let setNumber = musicIndex + 1;
       setMusicIndex(setNumber)
       updateCurrentMusicDetails(setNumber);
     }
   }
 
-  const handlePrevSong = ()=>{
+  const handlePrevSong = () => {
     if (musicIndex === 0) {
       let setNumber = musicAPI.length - 1;
       setMusicIndex(setNumber);
       updateCurrentMusicDetails(setNumber);
-    }else{
+    } else {
       let setNumber = musicIndex - 1;
       setMusicIndex(setNumber)
       updateCurrentMusicDetails(setNumber);
     }
   }
 
-  const updateCurrentMusicDetails = (number)=>{
+  const updateCurrentMusicDetails = (number) => {
     let musicObject = musicAPI[number];
     currentAudio.current.src = musicObject.songSrc;
     currentAudio.current.play();
@@ -130,30 +130,30 @@ function App() {
     setIsAudioPlaying(true);
   }
 
-  const handleAudioUpdate = ()=>{
+  const handleAudioUpdate = () => {
     //Input total length of the audio
     let minutes = Math.floor(currentAudio.current.duration / 60);
     let seconds = Math.floor(currentAudio.current.duration % 60);
-    let musicTotalLength0 = `${minutes <10 ? `0${minutes}` : minutes} : ${seconds <10 ? `0${seconds}` : seconds}`;
+    let musicTotalLength0 = `${minutes < 10 ? `0${minutes}` : minutes} : ${seconds < 10 ? `0${seconds}` : seconds}`;
     setMusicTotalLength(musicTotalLength0);
 
     //Input Music Current Time
     let currentMin = Math.floor(currentAudio.current.currentTime / 60);
     let currentSec = Math.floor(currentAudio.current.currentTime % 60);
-    let musicCurrentT = `${currentMin <10 ? `0${currentMin}` : currentMin} : ${currentSec <10 ? `0${currentSec}` : currentSec}`;
+    let musicCurrentT = `${currentMin < 10 ? `0${currentMin}` : currentMin} : ${currentSec < 10 ? `0${currentSec}` : currentSec}`;
     setMusicCurrentTime(musicCurrentT);
 
     const progress = parseInt((currentAudio.current.currentTime / currentAudio.current.duration) * 100);
-    setAudioProgress(isNaN(progress)? 0 : progress)
+    setAudioProgress(isNaN(progress) ? 0 : progress)
   }
 
 
-  const vidArray = ['./Assets/Videos/video1.mp4','./Assets/Videos/video2.mp4','./Assets/Videos/video3.mp4','./Assets/Videos/video4.mp4','./Assets/Videos/video5.mp4','./Assets/Videos/video6.mp4'];
+  const vidArray = ['./Assets/Videos/video1.mp4', './Assets/Videos/video2.mp4', './Assets/Videos/video3.mp4', './Assets/Videos/video4.mp4', './Assets/Videos/video5.mp4', './Assets/Videos/video6.mp4'];
 
-  const handleChangeBackground = ()=>{
+  const handleChangeBackground = () => {
     if (videoIndex >= vidArray.length - 1) {
       setVideoIndex(0);
-    }else{
+    } else {
       setVideoIndex(videoIndex + 1)
     }
   }
@@ -161,34 +161,39 @@ function App() {
 
   return (
     <>
-    <div className="container">
-      <audio src='./Assets/songs/Chasing - NEFFEX.mp3' ref={currentAudio} onEnded={handleNextSong} onTimeUpdate={handleAudioUpdate}></audio>
-      <video src={vidArray[videoIndex]} loop muted autoPlay className='backgroundVideo'></video>
-      <div className="blackScreen"></div>
-      <div className="music-Container">
-        <p className='musicPlayer'>Music Player</p>
-        <p className='music-Head-Name'>{currentMusicDetails.songName}</p>
-        <p className='music-Artist-Name'>{currentMusicDetails.songArtist}</p>
-        <img src={currentMusicDetails.songAvatar} className={avatarClass[avatarClassIndex]} onClick={handleAvatar} alt="song Avatar" id='songAvatar'/>
-        <div className="musicTimerDiv">
-          <p className='musicCurrentTime'>{musicCurrentTime}</p>
-          <p className='musicTotalLenght'>{musicTotalLength}</p>
+      <div className="container">
+        <audio src='./Assets/songs/Chasing - NEFFEX.mp3' ref={currentAudio} onEnded={handleNextSong} onTimeUpdate={handleAudioUpdate}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos impedit nesciunt, est sed aspernatur dolores hic, sunt voluptatibus fugiat consequuntur perspiciatis ullam perferendis, optio placeat sequi illo delectus consectetur aperiam.</audio>
+        <video src={vidArray[videoIndex]} loop muted autoPlay className='backgroundVideo'></video>
+        <div className="blackScreen"></div>
+        <div className="music-Container">
+          <p className='musicPlayer'>Music Player</p>
+          <p className='music-Head-Name'>{currentMusicDetails.songName}</p>
+          <p className='music-Artist-Name'>{currentMusicDetails.songArtist}</p>
+          <div className="song__img">
+            <img src={currentMusicDetails.songAvatar} className={avatarClass[avatarClassIndex]} onClick={handleAvatar} alt="song Avatar"  />
+          </div>
+
+
+          <div className="musicControlers">
+            <i className='fa-solid fa-backward musicControler' onClick={handlePrevSong}></i>
+            <i className={`fa-solid ${isAudioPlaying ? 'fa-pause-circle' : 'fa-circle-play'} playBtn`} onClick={handleAudioPlay}></i>
+            <i className='fa-solid fa-forward musicControler' onClick={handleNextSong}></i>
+          </div>
+          <div className="musicTimerDiv">
+            <p className='musicCurrentTime'>{musicCurrentTime}</p>
+            <p className='musicTotalLenght'>{musicTotalLength}</p>
+          </div>
+          <input type="range" name="musicProgressBar" className='musicProgressBar' value={audioProgress} onChange={handleMusicProgressBar} />
+
         </div>
-        <input type="range" name="musicProgressBar" className='musicProgressBar' value={audioProgress} onChange={handleMusicProgressBar} />
-        <div className="musicControlers">
-          <i className='fa-solid fa-backward musicControler' onClick={handlePrevSong}></i>
-          <i className={`fa-solid ${isAudioPlaying? 'fa-pause-circle' : 'fa-circle-play'} playBtn`} onClick={handleAudioPlay}></i>
-          <i className='fa-solid fa-forward musicControler' onClick={handleNextSong}></i>
+        <div className="changeBackBtn" onClick={handleChangeBackground}>
+          Change Background
         </div>
+        <a href="https://github.com/Fisayomiii/Advance-Music-Player-React-JS" title='Give me a Star' className='youtube-Subs'>
+          <img src="./Assets/Images/github.png" alt="Youtube Logo" />
+          <p>Github</p>
+        </a>
       </div>
-      <div className="changeBackBtn" onClick={handleChangeBackground}>
-        Change Background
-      </div>
-      <a href="https://www.youtube.com/@teenage-programmer" title='Subscribe' className='youtube-Subs'>
-        <img src="./Assets/Images/Youtube_logo.png" alt="Youtube Logo"/>
-        <p>Teenage Programmer</p>
-      </a>
-    </div>
     </>
   );
 }
